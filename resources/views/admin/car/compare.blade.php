b3ba115c360dd3ff48be20508a51ca937785cdbc
anggiirawan13 <irawananggi76@gmail.com>
2025-03-29 17:25:54 +0700

    feat: upgrade feature

--- resources/views/admin/car/compare.blade.php
+++ resources/views/admin/car/compare.blade.php
@@ -12,13 +12,13 @@
             <div class="card-body">
                 <div class="row">
                     <div class="col-md-6">
-                        <h4>Mobil 1: {{ $car1->name }}</h4>
-                        <img src="{{ $car1->image_path ? asset('storage/car/' . $car1->image_path) : asset('img/default-image.png') }}" class="img-fluid rounded shadow" alt="{{ $car1->name }}">
+                        <h4><strong>Mobil 1: {{ $car1->name }}</strong></h4>
+                        <img src="{{ $car1->image_path ? asset('storage/car/' . $car1->image_path) : asset('img/default-image.png') }}" class="img-fluid rounded shadow my-3" alt="{{ $car1->name }}">
                         <x-table_car :car="$car1" />
                     </div>
                     <div class="col-md-6">
-                        <h4>Mobil 2: {{ $car2->name }}</h4>
-                        <img src="{{ $car2->image_path ? asset('storage/car/' . $car2->image_path) : asset('img/default-image.png') }}" class="img-fluid rounded shadow" alt="{{ $car2->name }}">
+                        <h4><strong>Mobil 2: {{ $car2->name }}</strong></h4>
+                        <img src="{{ $car2->image_path ? asset('storage/car/' . $car2->image_path) : asset('img/default-image.png') }}" class="img-fluid rounded shadow my-3" alt="{{ $car2->name }}">
                         <x-table_car :car="$car2" />
                     </div>
                 </div>
