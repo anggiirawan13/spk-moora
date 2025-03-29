643045b83ae8b8c4d11a23098bc1e58bcb521504
anggiirawan13 <irawananggi76@gmail.com>
2025-03-30 00:14:49 +0700

    feat: upgrade feature

--- resources/views/admin/alternative/index.blade.php
+++ resources/views/admin/alternative/index.blade.php
@@ -7,18 +7,30 @@
     <x-table title="Daftar Alternatif" createRoute="alternative.create" showRoute="alternative.show"
         editRoute="alternative.edit" deleteRoute="alternative.destroy" :data="$alternatives" :columns="array_merge(
             [['label' => 'Nama', 'field' => 'name']],
-            $alternatives
-                ->map(
-                    fn($alt) => $criterias->map(
+            !$alternatives->isEmpty()
+                ? $alternatives
+                    ->map(
+                        fn($alt) => $criterias
+                            ->map(
+                                fn($criteria) => [
+                                    'label' => $criteria->name,
+                                    'field' => $alt->values->firstWhere('criteria_id', $criteria->id)->value ?? 0,
+                                    'php' => true,
+                                ],
+                            )
+                            ->toArray(),
+                    )
+                    ->flatten(1)
+                    ->toArray()
+                : $criterias
+                    ->map(
                         fn($criteria) => [
                             'label' => $criteria->name,
-                            'field' => $alt->values->firstWhere('criteria_id', $criteria->id)->value ?? 0,
+                            'field' => 'value_' . $criteria->id, // Buat field default agar tidak error
                             'php' => true,
-                        ]
-                    )->toArray()
-                )
-                ->flatten(1)
-                ->toArray()
+                        ],
+                    )
+                    ->toArray(),
         )" />
 
 @endsection
