6c48d578e962451a2fbf3c100472eca74c10a031
anggiirawan13 <irawananggi76@gmail.com>
2025-04-06 22:26:43 +0700

    feat: update dashboard

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -16,7 +16,7 @@
 
             <!-- Nav Item - Dashboard -->
             <li class="nav-item active">
-                <a class="nav-link" href="{{ route('admin.dashboard.index') }}">
+                <a class="nav-link" href="{{ route('dashboard.index') }}">
                     <i class="fas fa-tachometer-alt"></i>
                     <span>Dashboard</span>
                 </a>
