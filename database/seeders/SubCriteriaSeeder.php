181c52e8ef61f36f2398e85fd7236e8cfb8de057
anggiirawan13 <irawananggi76@gmail.com>
2025-04-23 01:01:59 +0700

    feat: adjust sub cri seed

--- database/seeders/SubCriteriaSeeder.php
+++ database/seeders/SubCriteriaSeeder.php
@@ -14,22 +14,22 @@ class SubCriteriaSeeder extends Seeder
             ['criteria_code' => 'C1', 'name' => '251 – 450 juta', 'value' => 1],
             ['criteria_code' => 'C1', 'name' => '351 – 450 juta', 'value' => 2],
             ['criteria_code' => 'C1', 'name' => '251 – 350 juta', 'value' => 3],
-            ['criteria_code' => 'C1', 'name' => '≤ 250 juta',    'value' => 4],
+            ['criteria_code' => 'C1', 'name' => '<= 250 juta',    'value' => 4],
 
             // C2 - Tahun Produksi (Benefit)
             ['criteria_code' => 'C2', 'name' => '2010 – 2014',   'value' => 1],
             ['criteria_code' => 'C2', 'name' => '2015 – 2017',   'value' => 2],
             ['criteria_code' => 'C2', 'name' => '2018 – 2020',   'value' => 3],
-            ['criteria_code' => 'C2', 'name' => '≥ 2021',        'value' => 4],
+            ['criteria_code' => 'C2', 'name' => '>= 2021',        'value' => 4],
 
             // C3 - Jarak Tempuh (Cost)
             ['criteria_code' => 'C3', 'name' => '100.001 – 150.000 km', 'value' => 1],
             ['criteria_code' => 'C3', 'name' => '50.001 – 100.000 km',  'value' => 2],
             ['criteria_code' => 'C3', 'name' => '20.001 – 50.000 km',   'value' => 3],
-            ['criteria_code' => 'C3', 'name' => '≤ 20.000 km',          'value' => 4],
+            ['criteria_code' => 'C3', 'name' => '<= 20.000 km',          'value' => 4],
 
             // C4 - Kapasitas Mesin (Benefit)
-            ['criteria_code' => 'C4', 'name' => '≤ 1000 cc',     'value' => 1],
+            ['criteria_code' => 'C4', 'name' => '<= 1000 cc',     'value' => 1],
             ['criteria_code' => 'C4', 'name' => '1001 – 1500',   'value' => 2],
             ['criteria_code' => 'C4', 'name' => '1501 – 2500',   'value' => 3],
             ['criteria_code' => 'C4', 'name' => '> 2500',        'value' => 4],
@@ -37,7 +37,7 @@ class SubCriteriaSeeder extends Seeder
             // C5 - Kapasitas Penumpang (Benefit)
             ['criteria_code' => 'C5', 'name' => '1 – 2', 'value' => 1],
             ['criteria_code' => 'C5', 'name' => '3 – 7', 'value' => 2],
-            ['criteria_code' => 'C5', 'name' => '≥ 8',   'value' => 3],
+            ['criteria_code' => 'C5', 'name' => '>= 8',   'value' => 3],
 
             // C6 - Transmisi (Benefit)
             ['criteria_code' => 'C6', 'name' => 'Manual',   'value' => 1],
